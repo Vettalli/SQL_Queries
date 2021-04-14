a63a9bf142cf908f35dabe43dc20075f521c3e8d
Vettalli <vettallioil@gmail.com>
2021-04-14 13:23:08 +0400

    no message

---
+++ 2)FillingTheTables.sql
@@ -0,0 +1,37 @@
+USE University;
+
+INSERT INTO COURSES
+VALUES
+(1,'Math-01','Equations'),
+(2,'Applied math','Statistics'),
+(3,'Russian literature','Novels, poems')
+
+INSERT INTO GROUPS
+VALUES
+(1,1,'SV-01'),
+(2,2,'SR-01'),
+(3,3,'SR-02')
+
+INSERT INTO STUDENTS
+VALUES
+(1,1,'Andy','Walkman'),
+(2,1,'Rust','Cole'),
+(3,1,'Woody','Woodpecker'),
+(4,1,'Bany','Skywalker'),
+(5,1,'Tedy','Deniels'),
+(6,1,'Neil','Druckman'),
+(7,1,'Marty','McFly'),
+(8,1,'Emmett','Brown'),
+(9,1,'Morty','Smith'),
+(10,2,'Joel','Miller'),
+(11,2,'Natan','Drake'),
+(12,2,'Elena','Fisher'),
+(13,2,'Jason','Bourne'),
+(14,2,'James','Sunderland'),
+(15,2,'Victor','Sullivan'),
+(16,2,'Ellie','Miller'),
+(17,3,'Bruce','Straley'),
+(18,3,'Troy','Baker'),
+(19,3,'Nolan','North'),
+(20,3,'David','Cage'),
+(21,3,'Alec','Holowka')
